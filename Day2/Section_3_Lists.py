a5398d3e1423568bb2222250f5f68b71405a65b2
HighandLight <>
2022-12-30 14:38:54 +0900

    add Day2_section_3 file.

---
+++ Day2/Section_3_Lists.py
@@ -0,0 +1,13 @@
+# List: Data Structure : 파이썬에서 데이터를 체계화하고 저장하는 방식
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(states_of_america[0])
+
+
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+print(dirty_dozen[-1])
+dirty_dozen[2] = "Tale"
+dirty_dozen.append("Sale")
+dirty_dozen.extend(["Angelaland", "Jack Bauer Land"])
+print(dirty_dozen)
