469756dd44610304e16a76e7a01955bbccf8b1ca
HighandLight <>
2022-12-30 14:28:16 +0900

    add day2_section_2 file

---
+++ Day2/Section_2_RandomDice.py
@@ -0,0 +1,17 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+coin = ['Head','Tails']
+result = random.choice(coin)
+
+print(result)
+
+'''
+random_side = random.randint(0,1)
+if random_side == 1:
+    print('Head')
+else:
+    print('Tails')
+'''
